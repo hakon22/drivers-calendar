6b077a79afd7bf1f91f57d5bb702028fa44bbd80
Aleksey <hakon1@mail.ru>
2024-08-18 02:57:34 +0300

    fix: редактирование бота

--- server/telegram/Telegram.ts
+++ server/telegram/Telegram.ts
@@ -4,18 +4,22 @@
 /* eslint-disable import/no-anonymous-default-export */
 /* eslint-disable class-methods-use-this */
 import { Request, Response } from 'express';
-import { Context } from 'telegraf';
+import { Context as TelegrafContext } from 'telegraf';
 import { Message } from 'typegram/message';
 import Users from '../db/tables/Users';
 import { telegramBotService } from '../server';
+
+interface Context extends TelegrafContext {
+  callbackQuery: TelegrafContext['callbackQuery'] & { data?: string },
+}
 
 class Telegram {
   public webhooks = async (req: Request, res: Response) => {
     try {
       const context = req.body as Context;
-      const message = context.message as Message.TextMessage;
+      const message = context.message as Message.ContactMessage & Message.TextMessage;
 
-      if (message?.text === '/start') {
+      const start = async () => {
         const replyMarkup = {
           keyboard: [
             [
@@ -23,19 +27,32 @@ class Telegram {
                 text: 'Отправить номер телефона',
                 request_contact: true,
               },
+              {
+                text: 'Рестарт',
+                callback_data: 'start_button',
+              },
             ],
           ],
           resize_keyboard: true,
           one_time_keyboard: true,
         };
         await telegramBotService.sendMessage('Пожалуйста, предоставьте ваш номер телефона:', message?.from?.id?.toString() as string, { reply_markup: replyMarkup });
-      } else if (message?.reply_to_message) {
-        const user = await Users.findOne({ where: { phone: message?.text } });
+      };
+
+      if (message?.text === '/start') {
+        await start();
+      } else if (message?.contact?.phone_number) {
+        const user = await Users.findOne({ where: { phone: message.contact.phone_number } });
         if (user) {
-          await Users.update({ telegramId: message?.from?.id?.toString() }, { where: { phone: message?.text } });
+          await Users.update({ telegramId: message?.from?.id?.toString() }, { where: { phone: message.contact.phone_number } });
           await telegramBotService.sendMessage('Вы успешно подписались на обновления.', message?.from?.id?.toString() as string);
         } else {
           await telegramBotService.sendMessage('Номер телефона не найден в базе данных.', message?.from?.id?.toString() as string);
+        }
+      } else if (context?.callbackQuery) {
+        const callData = context.callbackQuery?.data;
+        if (callData === 'start_button') {
+          await start();
         }
       } else if (context.myChatMember?.new_chat_member?.status === 'kicked') {
         const telegramId = context.myChatMember.chat.id;
