b7e6a706a09dd1f55b47c9355f9ce9ea9f80f7cd
Aleksey <hakon1@mail.ru>
2024-08-17 20:13:54 +0300

    feat: добавил оформление страницы 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,25 @@
 import { useTranslation } from 'react-i18next';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { Button, Result } from 'antd';
 import Helmet from '@/components/Helmet';
-import BackButton from '@/components/BackButton';
+import image404 from '@/images/404.svg';
 
 const Page404 = () => {
   const { t } = useTranslation('translation', { keyPrefix: '404' });
+  const router = useRouter();
+
+  const back = () => router.back();
 
   return (
     <div className="my-5 row d-flex justify-content-center">
-      <BackButton title={t('prev')} />
-      <div className="col-12 col-md-8">
-        <Helmet title={t('title')} description={t('description')} />
-        404
-      </div>
+      <Helmet title={t('title')} description={t('description')} />
+      <Result
+        icon={<Image src={image404} alt={t('title')} />}
+        title={t('title')}
+        subTitle={t('description')}
+        extra={<Button className="button col-6" onClick={back}>{t('prev')}</Button>}
+      />
     </div>
   );
 };
